aaef5694fbd388ebf52a5786d4ac9585c4ea8435
Seal Security <devops-admin@sealsecurity.io>
2024-07-22 11:35:43 +0000

    patched v1.1.8

--- test/api-test.js
+++ test/api-test.js
@@ -300,6 +300,15 @@ describe('IP library for node.js', () => {
       assert.equal(ip.isPrivate('::1'), true);
       assert.equal(ip.isPrivate('fe80::1'), true);
     });
+
+    it('CVE-2023-42282', () => {
+      assert.equal(ip.isPrivate('0x7f.1'), true);
+      assert.equal(ip.isPrivate('0x7f.0.1'), true);
+      assert.equal(ip.isPrivate('0x7f.0.0.1'), true);
+      assert.equal(ip.isPrivate('0177.1'), true);
+      assert.equal(ip.isPrivate('0177.0.1'), true);
+      assert.equal(ip.isPrivate('0177.0.0.1'), true);
+    });
   });
 
   describe('loopback() method', () => {
